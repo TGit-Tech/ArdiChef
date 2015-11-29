10755de85b829dd2db24df5a62be482fe76666bf
tgit23 <tgit23@yahoo.com>
2015-11-28 21:56:30 -0700

    EWS Visual Improvements

--- software/EWS/js/loadjson.js
+++ software/EWS/js/loadjson.js
@@ -1,4 +1,6 @@
 window.onload = function () {
+    
+    // Setup a AJAJ request object
     var xhttp;
     if(window.XMLHttpRequest) {
         xhttp = new XMLHttpRequest();
@@ -6,9 +8,12 @@ window.onload = function () {
         xhttp = new ActiveXObject("Microsoft.XMLHTTP");
     }
     
+    // Load the <html>.JSON file
     var url = window.location.pathname;                         // Get URL
     var filename = url.substring(url.lastIndexOf('/')+1);       // Get HTML page filename
     filename = filename.slice(0,filename.lastIndexOf('.'));     // Remove Extension
+    xhttp.open("GET", "json/" + filename + ".json", true);      // Load equivalent JSON file
+    xhttp.send();
     
     xhttp.onreadystatechange = function () {
         var out = "<br><br><table id=\"content\">";
@@ -31,7 +36,7 @@ window.onload = function () {
             }
             else if(filename=="recipes"){
                 for( i in obj.recipes ) {
-                    out = out + "<th id=\"content\" >" + obj.recipes[i].name + "</th>";
+                    out = out + "<td id=\"content\" ><div id=\"arecipe\">" + obj.recipes[i].attribute + " " + obj.recipes[i].name + "</div>";
                     var ingr = "";
                     // Build Recipe Items
                     for ( j in obj.recipes[i].ingredients ) {
@@ -40,33 +45,31 @@ window.onload = function () {
                             obj.recipes[i].ingredients[j].unit + " " +
                             obj.recipes[i].ingredients[j].name + "<br>";
                     }
-                    
-                    // Attach Recipe's Column Details
-                    col.push( obj.recipes[i].attribute + "<br />" +
-                        "<div " +
+                    out = out + "<div style=\"width: 10px;\" " +
                         "onmouseover=\"document.getElementById('recipe" + i + "').style.display = 'inline';\" " +
                         "onmouseout=\"document.getElementById('recipe" + i + "').style.display = 'none';\"><i>Recipe</i></div>" +
-                        "<div id=\"recipe" + i + "\" style=\"background-color:yellow;display:none;\">" + ingr + "</div>");
+                        "<div id=\"recipe" + i + "\" style=\"background-color:yellow;display:none;\">" + ingr + "</div></td>"
                 }
             }
             
+            out = out + "<tr>";
+            for( r in col ) { out = out + "<td id=\"content\">" + col[r] + "</td>"; }
+            document.getElementById("content").innerHTML = out; 
+            out = out + "</tr></table>";
+            
+            // Add recipe details
+            if (filename=="recipes") {
+                for( var r = 0; r < rec.length; ++r ) {
+                    if ( document.getElementById("recipe" + r) != null) {
+                        document.getElementById("recipe" + r).innerHTML = rec[r];
+                    }  //Load recipes
+                }
+            }           
+             
         }
-        out = out + "<tr>";
-        for( r in col ) { out = out + "<td id=\"content\">" + col[r] + "</td>"; }
-        document.getElementById("content").innerHTML = out; 
-        out = out + "</tr></table>";
-        
-        // Add recipe details
-        if (filename=="recipes") {
-            for( var r = 0; r < rec.length; ++r ) {
-                if ( document.getElementById("recipe" + r) != null) {
-                    document.getElementById("recipe" + r).innerHTML = rec[r];
-                }  //Load recipes
-            }
-        }
+
         
     };
 
-    xhttp.open("GET", "json/" + filename + ".json", true);      // Load equivalent JSON file
-    xhttp.send();
+
 }
